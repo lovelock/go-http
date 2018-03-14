c2e47618cb740ff1ca5f55e4bc657fd7a2ac5375
Frost Wong <frostwong@gmail.com>
2018-03-14 16:31:55 +0800

    use http.NewServerMux

--- main.go
+++ main.go
@@ -5,20 +5,34 @@ import (
 	"fmt"
 )
 
-type foo struct{}
-
-func (f foo) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/boy":
-		fmt.Fprintln(w, "I love you!!!")
-	case "/girl":
-		fmt.Fprintln(w, "hehe")
-	default:
-		fmt.Fprintln(w, "Men would stop talking and women would shed tears when they see this. 男默女泪啊哈哈")
+type (
+	boy struct{}
+	girl struct {
 	}
+	foo struct {
+	}
+)
+
+func (b boy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "I love you!!!")
+
+}
+func (g girl) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "hehe")
+
+}
+func (f foo) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Men would stop talking and women would shed tears when they see this. 男默女泪啊哈哈")
 }
 
 func main() {
-	f := foo{}
-	http.ListenAndServe(":8080", f)
+
+	b, g, f := boy{}, girl{}, foo{}
+
+	mux := http.NewServeMux()
+	mux.Handle("/boy", b)
+	mux.Handle("/girl", g)
+	mux.Handle("/", f)
+
+	http.ListenAndServe(":8080", mux)
 }
