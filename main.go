8ed83632cb546ea83a796af6db2cca5fbf848996
Frost Wong <frostwong@gmail.com>
2018-03-14 16:03:11 +0800

    emulate an http server

--- main.go
+++ main.go
@@ -20,8 +20,21 @@ func main() {
 			log.Fatal(err)
 		}
 
-		fmt.Fprintln(conn, "Hello from TCP server")
-
-		conn.Close()
+		handle(conn)
 	}
+}
+func handle(conn net.Conn) {
+	defer conn.Close()
+
+	respond(conn)
+}
+
+func respond(conn net.Conn) {
+	body := `<!DOCTYPE html><html><head><meta charset="UTF-8"><title>TCP Example</title></head><body>Hello from TCP server</body></html>`
+
+	fmt.Fprint(conn, "HTTP/1.1 200 OK\r\n")
+	fmt.Fprintf(conn, "Content-Length: %d\r\n", len(body))
+	fmt.Fprint(conn, "Content-Type: text/html\r\n")
+	fmt.Fprint(conn, "\r\n")
+	fmt.Fprint(conn, body)
 }
