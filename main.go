cfe108f24ef8a80cabc5a67474454416e8ec8fdc
Frost Wong <frostwong@gmail.com>
2018-03-14 16:35:06 +0800

    use handle function instead of Handler

--- main.go
+++ main.go
@@ -5,33 +5,23 @@ import (
 	"fmt"
 )
 
-type (
-	boy struct{}
-	girl struct {
-	}
-	foo struct {
-	}
-)
-
-func (b boy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func boy(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "I love you!!!")
 
 }
-func (g girl) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func girl(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "hehe")
 
 }
-func (f foo) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func foo(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Men would stop talking and women would shed tears when they see this. 男默女泪啊哈哈")
 }
 
 func main() {
 
-	b, g, f := boy{}, girl{}, foo{}
-
-	http.Handle("/boy", b)
-	http.Handle("/girl", g)
-	http.Handle("/", f)
+	http.HandleFunc("/boy", boy)
+	http.HandleFunc("/girl", girl)
+	http.HandleFunc("/", foo)
 
 	http.ListenAndServe(":8080", nil)
 }
