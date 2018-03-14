f82fa2ed00570a27da91ba85f59b7d691baf063e
Frost Wong <frostwong@gmail.com>
2018-03-14 16:16:08 +0800

    simplest http server use http

--- main.go
+++ main.go
@@ -1,41 +1,7 @@
 package main
 
-import (
-	"net"
-	"log"
-	"fmt"
-)
+import "net/http"
 
 func main() {
-	listen, err := net.Listen("tcp", ":8080")
-	if err != nil {
-		log.Panic(err)
-	}
-
-	defer listen.Close()
-
-	for {
-		conn, err := listen.Accept()
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		go handle(conn)
-	}
-}
-func handle(conn net.Conn) {
-	defer conn.Close()
-
-	respond(conn)
-}
-
-func respond(conn net.Conn) {
-	body := `<!DOCTYPE html><html><head><meta charset="UTF-8"><title>TCP Example</title></head><body>Hello from TCP server</body></html>`
-
-	fmt.Fprint(conn, "HTTP/1.1 200 OK\r\n")
-	fmt.Fprintf(conn, "Content-Length: %d\r\n", len(body))
-	fmt.Fprint(conn, "Content-Type: text/html\r\n")
-	fmt.Fprint(conn, "Server: fuck server\r\n")
-	fmt.Fprint(conn, "\r\n")
-	fmt.Fprint(conn, body)
+	http.ListenAndServe(":8080", nil)
 }
