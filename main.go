479257e2504b1e94c3e4313204576a7786a902f2
Frost Wong <frostwong@gmail.com>
2018-03-14 16:26:59 +0800

    handle different path in one handler

--- main.go
+++ main.go
@@ -8,7 +8,14 @@ import (
 type foo struct{}
 
 func (f foo) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Implement the Handler interface")
+	switch r.URL.Path {
+	case "/boy":
+		fmt.Fprintln(w, "I love you!!!")
+	case "/girl":
+		fmt.Fprintln(w, "hehe")
+	default:
+		fmt.Fprintln(w, "Men would stop talking and women would shed tears when they see this. 男默女泪啊哈哈")
+	}
 }
 
 func main() {
